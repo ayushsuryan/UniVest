463efdd254c87b2edc80fe7567772462efb505ec
Ayush Kumar Suryan <100873752+ayushsuryan@users.noreply.github.com>
2025-06-17 18:46:36 +0530

    Basic Navigation

---
+++ src/Pages/Signup.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Signup = ({ navigation }: { navigation: any }) => {
+  return (
+    <Text>Signup</Text>
+  );
+};
+
+export default Signup;
